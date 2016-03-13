3f2a0f84ed2fe4a5f705965b4aee51def79d9210
aydon@home <aydon520@gmail.com>
2016-03-13 11:05:08 +0800

    修改监听端口为可自定义

--- server.py
+++ server.py
@@ -1,16 +1,17 @@
 #coding:utf-8
 
 import tornado.ioloop
-import sys
+import tornado.httpserver
+import tornado.options
+import tornado.web
+from tornado.options import define, options
+define("port", default=8000, help="run on the given port", type=int)
 
 from application import application
 
-PORT = '8080'
 
 if __name__ == "__main__":
-  if len(sys.argv) > 1:
-    PORT = sys.argv[1]
-  application.listen(PORT)
-  print 'Development server is running at http://127.0.0.1:%s/' % PORT
+  application.listen(options.port)
+  print 'Development server is running at http://127.0.0.1:%s/' % options.port
   print 'Quit the server with CONTROL-C'
   tornado.ioloop.IOLoop.instance().start()
